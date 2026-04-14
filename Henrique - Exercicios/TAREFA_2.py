e120a9a423d7adef1982bc1f9f7ae926a427e4d0
Henrique Merigo <henrimerigo1000@gmail.com>
2026-04-13 21:12:52 -0300

    Correção pontual

--- Henrique - Exercicios/TAREFA_2.py
+++ Henrique - Exercicios/TAREFA_2.py
@@ -4,14 +4,16 @@
 # Juvenil: de 11 a 17 anos.
 # Adulto: 18 anos ou mais.
 # Dica: Se a idade for menor que 5, o programa deve dizer que ele ainda não pode competir.
+# ================================================================================================================================================
+import os # import utiliza uma biblioteca externa. Usamos "os" para limpar o terminal automaticamente sempre que rodamos o codigo 
+os.system('cls') # Essa ferramenta limpa a tela sempre que for executada
+idade = int(input("Digite a idade: ")) # Declaramos a variavel "idade" para receber o input do cliente, convertendo para inteiro para comparar com as faixas etárias das categorias de natação
 
-idade = int(input("Digite a idade: "))
-
-if idade >= 5 and idade <= 10:
+if idade >= 5 and idade <= 10: # O if compara a variavel "idade" com as faixas etárias das categorias de natação e printa a categoria correspondente ou uma mensagem de erro caso a idade seja menor que 5
     print("Infantil")
-elif idade >= 11 and idade <= 17:
+elif idade >= 11 and idade <= 17: # O elif compara a variavel "idade" com as faixas etárias das categorias de natação e printa a categoria correspondente ou uma mensagem de erro caso a idade seja menor que 5
     print("Juvenil")
 elif idade >= 18:
     print("Adulto")
-else:
+else: # O else é executado caso a variavel "idade" seja menor que 5, printando a mensagem de erro para o cliente
     print("Atleta nao pode competir.")
